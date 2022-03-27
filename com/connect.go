4b24f07f0090a61c44893b7aafdde73da8d9a46d
Elvis Rodrigues <elvis@elrodrigues.com>
2022-03-26 22:24:53 -0400

    start interface to daemon

--- com/connect.go
+++ com/connect.go
@@ -2,11 +2,16 @@ package com
 
 // To be implemented
 import (
-	"github.com/hashicorp/memberlist"
+	"fmt"
+	// "github.com/hashicorp/memberlist"
 )
 
 // options to be added
-func Init() (*memberlist.Memberlist, error) {
-	list, err := memberlist.Create(memberlist.DefaultLANConfig())
-	return list, err
+// func Init() (*memberlist.Memberlist, error) {
+// 	list, err := memberlist.Create(memberlist.DefaultLANConfig())
+// 	return list, err
+// }
+
+func Pass() {
+	fmt.Println("Filler Function")
 }
